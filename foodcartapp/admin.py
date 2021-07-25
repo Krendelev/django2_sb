391b6f55b3fdf837c60ebfae7092cbb74db2a69b
Krendelev <krendelev@gmail.com>
2021-07-25 15:47:05 +0300

    add models for order data

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -3,10 +3,15 @@ from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
 
-from .models import Product
-from .models import ProductCategory
-from .models import Restaurant
-from .models import RestaurantMenuItem
+from .models import (
+    Banner,
+    Order,
+    OrderItem,
+    Product,
+    ProductCategory,
+    Restaurant,
+    RestaurantMenuItem,
+)
 
 
 class RestaurantMenuItemInline(admin.TabularInline):
@@ -14,93 +19,126 @@ class RestaurantMenuItemInline(admin.TabularInline):
     extra = 0
 
 
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
 @admin.register(Restaurant)
 class RestaurantAdmin(admin.ModelAdmin):
     search_fields = [
-        'name',
-        'address',
-        'contact_phone',
+        "name",
+        "address",
+        "contact_phone",
     ]
     list_display = [
-        'name',
-        'address',
-        'contact_phone',
+        "name",
+        "address",
+        "contact_phone",
     ]
-    inlines = [
-        RestaurantMenuItemInline
-    ]
+    inlines = [RestaurantMenuItemInline]
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = [
-        'get_image_list_preview',
-        'name',
-        'category',
-        'price',
+        "get_image_list_preview",
+        "name",
+        "category",
+        "price",
     ]
     list_display_links = [
-        'name',
+        "name",
     ]
     list_filter = [
-        'category',
+        "category",
     ]
     search_fields = [
         # FIXME SQLite can not convert letter case for cyrillic words properly, so search will be buggy.
         # Migration to PostgreSQL is necessary
-        'name',
-        'category__name',
+        "name",
+        "category__name",
     ]
 
-    inlines = [
-        RestaurantMenuItemInline
-    ]
+    inlines = [RestaurantMenuItemInline]
     fieldsets = (
-        ('Общее', {
-            'fields': [
-                'name',
-                'category',
-                'image',
-                'get_image_preview',
-                'price',
-            ]
-        }),
-        ('Подробно', {
-            'fields': [
-                'special_status',
-                'description',
-            ],
-            'classes': [
-                'wide'
-            ],
-        }),
+        (
+            "Общее",
+            {
+                "fields": [
+                    "name",
+                    "category",
+                    "image",
+                    "get_image_preview",
+                    "price",
+                ]
+            },
+        ),
+        (
+            "Подробно",
+            {
+                "fields": [
+                    "special_status",
+                    "description",
+                ],
+                "classes": ["wide"],
+            },
+        ),
     )
 
     readonly_fields = [
-        'get_image_preview',
+        "get_image_preview",
     ]
 
     class Media:
-        css = {
-            "all": (
-                static("admin/foodcartapp.css")
-            )
-        }
+        css = {"all": (static("admin/foodcartapp.css"))}
 
     def get_image_preview(self, obj):
         if not obj.image:
-            return 'выберите картинку'
-        return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
-    get_image_preview.short_description = 'превью'
+            return "выберите картинку"
+        return format_html(
+            '<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url
+        )
+
+    get_image_preview.short_description = "превью"
 
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
-            return 'нет картинки'
-        edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
-    get_image_list_preview.short_description = 'превью'
+            return "нет картинки"
+        edit_url = reverse("admin:foodcartapp_product_change", args=(obj.id,))
+        return format_html(
+            '<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>',
+            edit_url=edit_url,
+            src=obj.image.url,
+        )
+
+    get_image_list_preview.short_description = "превью"
 
 
 @admin.register(ProductCategory)
 class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    search_fields = [
+        "last_name",
+        "address",
+        "phone",
+    ]
+    list_display = [
+        "get_customer",
+        "address",
+        "customer_phone",
+    ]
+    inlines = [OrderItemInline]
+
+    @admin.display(description="Клиент")
+    def get_customer(self, obj):
+        return f"{obj.last_name} {obj.first_name}"
+
+
+@admin.register(Banner)
+class BannerAdmin(admin.ModelAdmin):
     pass
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,62 +1,70 @@
-from django.http import JsonResponse
+import json
+
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.templatetags.static import static
 
 
-from .models import Product
+from .models import Banner, Product, Order, OrderItem
 
 
 def banners_list_api(request):
-    # FIXME move data to db?
-    return JsonResponse([
-        {
-            'title': 'Burger',
-            'src': static('burger.jpg'),
-            'text': 'Tasty Burger at your door step',
-        },
-        {
-            'title': 'Spices',
-            'src': static('food.jpg'),
-            'text': 'All Cuisines',
-        },
-        {
-            'title': 'New York',
-            'src': static('tasty.jpg'),
-            'text': 'Food is incomplete without a tasty dessert',
-        }
-    ], safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    banners = [
+        {"title": obj.title, "src": obj.image.url, "text": obj.tagline}
+        for obj in Banner.objects.all()
+    ]
+    return JsonResponse(
+        banners, safe=False, json_dumps_params={"ensure_ascii": False, "indent": 4}
+    )
 
 
 def product_list_api(request):
-    products = Product.objects.select_related('category').available()
+    products = Product.objects.select_related("category").available()
 
     dumped_products = []
     for product in products:
         dumped_product = {
-            'id': product.id,
-            'name': product.name,
-            'price': product.price,
-            'special_status': product.special_status,
-            'description': product.description,
-            'category': {
-                'id': product.category.id,
-                'name': product.category.name,
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+            "special_status": product.special_status,
+            "description": product.description,
+            "category": {
+                "id": product.category.id,
+                "name": product.category.name,
             },
-            'image': product.image.url,
-            'restaurant': {
-                'id': product.id,
-                'name': product.name,
-            }
+            "image": product.image.url,
+            "restaurant": {
+                "id": product.id,
+                "name": product.name,
+            },
         }
         dumped_products.append(dumped_product)
-    return JsonResponse(dumped_products, safe=False, json_dumps_params={
-        'ensure_ascii': False,
-        'indent': 4,
-    })
+    return JsonResponse(
+        dumped_products,
+        safe=False,
+        json_dumps_params={
+            "ensure_ascii": False,
+            "indent": 4,
+        },
+    )
 
 
 def register_order(request):
-    # TODO это лишь заглушка
+    try:
+        order_serialized = json.loads(request.body.decode())
+    except ValueError:
+        return HttpResponseBadRequest
+
+    order = Order.objects.create(
+        first_name=order_serialized["firstname"],
+        last_name=order_serialized["lastname"],
+        address=order_serialized["address"],
+        customer_phone=order_serialized["phonenumber"],
+    )
+    for item in order_serialized["products"]:
+        OrderItem.objects.get_or_create(
+            order=order,
+            product=Product.objects.get(pk=item["product"]),
+            quantity=item["quantity"],
+        )
     return JsonResponse({})
